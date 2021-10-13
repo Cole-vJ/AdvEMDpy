55d3bed0cff0f93f3b6363a98f28a2ea360b1f42
Cole vJ <colevj0303@gmail.com>
2021-10-13 12:51:02 +0100

    Some small notation added.

--- __init__.py
+++ __init__.py
@@ -5,9 +5,9 @@
 #       \  /
 #        \/
 
-from AdvEMDpy import EMD
-from emd_basis import Basis
-from emd_hilbert import Hilbert, hilbert_spectrum
-from emd_mean import Fluctuation
-from emd_preprocess import Preprocess
-from emd_utils import Utility
+from AdvEMDpy.AdvEMDpy import EMD
+from AdvEMDpy.emd_basis import Basis
+from AdvEMDpy.emd_hilbert import Hilbert, hilbert_spectrum
+from AdvEMDpy.emd_mean import Fluctuation
+from AdvEMDpy.emd_preprocess import Preprocess
+from AdvEMDpy.emd_utils import Utility
